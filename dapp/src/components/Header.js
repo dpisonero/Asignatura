60cf24097d2c6f5aea9e72eb3d2f5925aaed0d95
dpisonero <davidpisonero96@gmail.com>
2020-12-27 13:51:39 +0100

    Header añadido

--- dapp/src/components/Header.js
+++ dapp/src/components/Header.js
@@ -1,7 +1,26 @@
-const Header = () => (
+import {newContextComponents} from "@drizzle/react-components";
+const {ContractData} = newContextComponents;
+
+const Header = props => (
     <header className="AppHeader">
         <h1>
-            P3 - Asignatura
+            P3 - Asignatura: &nbsp;
+            <ContractData
+                drizzle={props.drizzle}
+                drizzleState={props.drizzleState}
+                contract={"Asignatura"}
+                method={"nombre"}
+            />
+            -
+            <ContractData
+                drizzle={props.drizzle}
+                drizzleState={props.drizzleState}
+                contract={"Asignatura"}
+                method={"curso"}
+                render={data => (
+                    <em>{data}</em>
+                )}
+            />
         </h1>
     </header>
 );
