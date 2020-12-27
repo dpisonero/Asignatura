95985d4831cea527d414b7d623c980d98e955b68
dpisonero <davidpisonero96@gmail.com>
2020-12-27 12:16:12 +0100

    Proyecto Drizzle

--- dapp/src/components/App.js
+++ dapp/src/components/App.js
@@ -1,12 +1,26 @@
+import {DrizzleContext} from "@drizzle/react-plugin";
+
 import '../css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>P3 - Asignatura</h1>
-      </header>
-    </div>
+      <DrizzleContext.Consumer>
+          {drizzleContext => {
+              const {drizzle, drizzleState, initialized} = drizzleContext;
+
+              if (!initialized) {
+                  return (<main><h1>⚙️Cargando dapp...</h1></main>);
+              }
+
+              return (
+                  <div className="App">
+                      <header className="App-header">
+                          <h1>P3 - Asignatura</h1>
+                      </header>
+                  </div>
+              ) ;
+          }}
+      </DrizzleContext.Consumer>
   );
 }
 
