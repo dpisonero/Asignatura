8a148a1699f2e62ecf1f564c64586cc4e57debea
dpisonero <davidpisonero96@gmail.com>
2020-12-27 11:58:22 +0100

    Proyecto React

---
+++ dapp/src/components/App.js
@@ -0,0 +1,13 @@
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>P3 - Asignatura</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
